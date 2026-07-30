4886b5464b6507d2eeae3c6292b6351654810a19
Alon Zeltser <alonzeltser1@gmail.com>
2026-07-30 15:35:37 +0300

    notebooks preperation update

--- src/general_utils.py
+++ src/general_utils.py
@@ -14,15 +14,3 @@ BRAIN_PLANES = {
 
 DL_SPLITS= ("train", "test", "val")
 
-def prepare_environment(hpc:bool):
-
-
-    if hpc:
-        data_path = 'MRI_2026_datasets/brain_age'
-    else:
-        #set here local relative path to the data set
-        data_path = '../../../../../../../mri_dataset/brain_age'
-
-    data_path = os.path.abspath(data_path)
-    print(f'Data path: {data_path}')
-    os.chdir(data_path)
